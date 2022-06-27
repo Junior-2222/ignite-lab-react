a44d7f3ab4b99711592132359121573dcbd3c55f
Junior-2222 <evolutionindie@gmail.com>
2022-06-27 01:51:10 -0300

    bug:img

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import mockupImage from '../assets/code-mockup.png';
 
 export function Subscribe() {
   const navigate = useNavigate();
@@ -70,7 +71,7 @@ export function Subscribe() {
         </div>
       </div>
 
-      <img src="/src/assets/code-mockup.png" className="mt-[40rem]" alt="" />
+      <img src={mockupImage} className="mt-[40rem]" alt="" />
     </div>
   );
 }
